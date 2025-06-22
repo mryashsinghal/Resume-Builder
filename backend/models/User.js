3122e977f1676e93718a95b1979ece0b354aa39c
NAVNEET SINGH <nsh12727@gmail.com>
2025-06-22 19:23:11 +0530

    added github and google authentication

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
+  displayName: { type: String, required: false },
+  googleId: { type: String, unique: true, sparse: true },
+  githubId: { type: String, unique: true, sparse: true },
+  createdAt: { type: Date, default: Date.now },
+  lastLogin: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
