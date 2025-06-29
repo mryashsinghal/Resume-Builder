f0a291b97ec5b488853990495cd60e5178c60070
Yash Singhal <152426138+mryashsinghal@users.noreply.github.com>
2025-06-29 12:40:12 +0530

    Revert "added google and GitHub authentication "

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email:    { type: String, required: true, unique: true },
-  password: { type: String, required: false },
-  displayName: { type: String, required: false },
-  googleId: { type: String, unique: true, sparse: true },
-  githubId: { type: String, unique: true, sparse: true },
-  createdAt: { type: Date, default: Date.now },
-  lastLogin: { type: Date, default: Date.now }
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
